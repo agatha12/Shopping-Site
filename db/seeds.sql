6b02c20c8f687905cbf083afd4b37093f0e4f416
agatha12 <agkiecun@gmail.com>
2018-12-11 18:59:57 -0500

    update 1

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+insert into products (id, name, price, img, designer, category)
+values(001, "Aura Goddess Duster Gown", 50, "https://i.ibb.co/jL4KKF6/auragoddessdustergown.jpg", "Kasia Kulenty", "apparel"),
+(002, "Aphrodite Goddess Gown", 50, "https://i.ibb.co/tB7nmZw/kasiaaphroditegoddessgown.jpg", "Kasia Kulenty", "apparel"),
+(003, "Athena Goddess Gown", 50, "https://i.ibb.co/KNPDBwX/kasiaathena.jpg", "Kasia Kulenty", "apparel"),
+(004, "Gaya Goddess Duster", 50, "https://i.ibb.co/kDTxJR4/kasiagayagoddessduster.jpg", "Kasia Kulenty", "apparel"),
+(005, "Helena Goddess Kaftan Gown", 50, "https://i.ibb.co/P6r5Ckh/kasiahelenagoddesskaftangown.jpg", "Kasia Kulenty", "apparel"),
+(006, "Elena Goddess Gown", 50, "https://i.ibb.co/0t18Fcn/kasiaselenagoddessgown.jpg", "Kasia Kulenty", "apparel"),
+(007, "Aphrodite Goddess Gown", 50, "https://i.ibb.co/Cm6d0gP/kasiavenus.jpg", "Kasia Kulenty", "apparel"),
+(008, "Leather Cap Arena", 40, "https://i.ibb.co/fqP1sM3/paxacaparena.jpg", "Paxa", "hats"),
+(009, "Leather Cap Black", 40, "https://i.ibb.co/kGMdxPv/paxacapblack.jpg", "Paxa", "hats"),
+(010, "Leather Cap Brown", 40, "https://i.ibb.co/JR4HWHH/paxacapbrown.jpg", "Paxa", "hats"),
+(011, "Onsie Yellow", 45, "https://i.ibb.co/vZCFLKS/yellowonesie.png", "Onsies for Oneness", "apparel"),
+(012, "Onsie Brown", 45, "https://i.ibb.co/PZ4Zdyf/brownonsie.png", "Onsies for Oneness", "apparel"),
+(013, "Onsie Black", 45, "https://i.ibb.co/YP86ktQ/blackonsie.png", "Onsies for Oneness", "apparel");
