7685327a13c61d2b1a7e93d23de298906cdc7752
unknown <juradomartinbenedict@gmail.com>
2018-12-11 19:57:44 -0500

    updated

--- db/seeds.sql
+++ db/seeds.sql
@@ -11,4 +11,23 @@ values(001, "Aura Goddess Duster Gown", 50, "https://i.ibb.co/jL4KKF6/auragoddes
 (010, "Leather Cap Brown", 40, "https://i.ibb.co/JR4HWHH/paxacapbrown.jpg", "Paxa", "hats"),
 (011, "Onsie Yellow", 45, "https://i.ibb.co/vZCFLKS/yellowonesie.png", "Onsies for Oneness", "apparel"),
 (012, "Onsie Brown", 45, "https://i.ibb.co/PZ4Zdyf/brownonsie.png", "Onsies for Oneness", "apparel"),
-(013, "Onsie Black", 45, "https://i.ibb.co/YP86ktQ/blackonsie.png", "Onsies for Oneness", "apparel");
+(013, "Onsie Black", 45, "https://i.ibb.co/YP86ktQ/blackonsie.png", "Onsies for Oneness", "apparel"),
+(014, "Rose Print Silk Maxi Dress", 446, "https://i.ibb.co/gMpMH9n/13071973-14999908-1000.jpg", "Natasha Zinko", "apparel"),
+(015, "Twisted and cut-out midi dress", 451, "https://i.ibb.co/ZzgVGjq/13221387-14662020-1000.jpg", "Supriya Lele", "apparel"), 
+(017, "Sea Nymph Ruffle Dress", 2000, "https://i.ibb.co/cDByGqn/13094707-14719365-480.jpg", "Johanna Ortiz", "apparel"),
+(018, "Kocchar Floral Mini Dress", 313, "https://i.ibb.co/72WMqsv/13173144-14890414-1000.jpg", "Ganni", "apparel"),
+(019, "Jane Keyhole Silk Dress", 451, "https://i.ibb.co/pXmLzrm/13185011-15190802-1000.jpg", "De La Vali", "apparel"),
+(020, "Leopard Faux Fur Coat", 440, "https://i.ibb.co/QvXJ9hz/13274055-14740243-1000.jpg", "Stand", "outerwear"),
+(021, "Checked Trench Coat", 1465, "https://i.ibb.co/5nzB9xL/13198922-15085798-1000.jpg", "Rokh", "outerwear"),
+(022, "Wool Blend Coat", 733, "https://i.ibb.co/FHpNFzL/12989059-14257010-480.jpg", "MSGM", "outerwear"),
+(023, "Checked Cotton Coat", 432, "https://i.ibb.co/svh15ND/13216928-15221312-1000.jpg", "Mara Hoffman", "outerwear"),
+(024, "Check Wool Trench Coat", 242, "https://i.ibb.co/nzyHjfw/13149102-14794514-1000.jpg", "Materiel", "outerwear"),
+(025, "Slingback Metal Heel Pumps", 350, "https://i.ibb.co/Cw8YM2H/13164528-15740053-1000.jpg","Yuul Yie", "shoes"),
+(026, "Leopard Print Fabric Flat Loafers", 221, "https://i.ibb.co/JtwgzKT/12972237-14499284-1000.jpg", "Re/Done", "shoes"),
+(027, "Leather Ankle Boots", 706, "https://i.ibb.co/TcHyF1d/13078731-14557619-1000.jpg", "Neos", "shoes"),
+(028, "Leather Biker Ankle Boots", 445, "https://i.ibb.co/Z2jwFLF/13343399-15369070-1000.jpg", "By Far", "shoes"),
+(029, "Crocodile Embossed Mules", 303, "https://i.ibb.co/dBnvKT3/13399237-15332287-1000.jpg", "ATP Atelier", "shoes"),
+(030, "Margot Tote Bag", 332, "https://i.ibb.co/Cm0qHwq/13339133-15069641-1000.jpg", "Danse Lete", "accessories"),
+(031, "Pink Fey Velvet Box Bag", 353, "https://i.ibb.co/b2YBs39/13083663-14641911-1000.jpg", "Mehry Miu", "accessories");
+
+
