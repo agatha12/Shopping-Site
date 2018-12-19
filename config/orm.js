d7eadb892817c3c1aa1eb6f5b3271d1423d1f8e5
SiennaS <ssienna48@gmail.com>
2018-12-18 19:10:57 -0500

    fixed var orm + and schema table add on

--- config/orm.js
+++ config/orm.js
@@ -150,6 +150,23 @@ var orm = {
 
       cb(result);
     });
+  },
+
+  // passport addition selectWhere and Create
+  selectWhere: function (tableInput, colToSearch, valOfCol, cb) {
+    var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+    connection.query(queryString, [tableInput, colToSearch, valOfCol], function (err, result) {
+      if (err) throw err;
+      cb(err, result)
+    });
+  },
+  create: function (tableInput, cols_vals, cb) {
+    var queryString = "INSERT INTO ?? set ?";
+    connection.query(queryString, [tableInput, cols_vals], function (err, result) {
+      if (err) throw err;
+      cb(err, result)
+    });
+
   }
 };
 
