e5e47283bf47ef1a9adcce584f3d150aca4f1f90
SiennaS <ssienna48@gmail.com>
2018-12-20 15:02:00 -0500

    connection, orm, dependencies, server.js

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 var express = require("express");
+var bodyParser = require("body-parser");
+var session = require("express-session");
+var passport = require("./config/passport");
 
 var PORT = process.env.PORT || 8080;
 
+// Creating express app and configuring middleware needed for authentication
 var app = express();
-
-app.use(express.static("public"));
-
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+// initialize passport
+app.use(passport.initialize());
+// initialize passport session
+app.use(passport.session());
+
+// Requiring our routes
+//require these files, and pass the express app to it
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
 
+// Express-Handlebars
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
@@ -22,7 +36,7 @@ var routes = require("./controllers/controller");
 app.use(routes);
 
 
-app.listen(PORT, function() {
 
-  console.log("Server listening on: http://localhost:" + PORT);
+app.listen(PORT, function () {
+  console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
 });
